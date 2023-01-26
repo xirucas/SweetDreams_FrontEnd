48cc99b642d9bd55e21b1c746a1ea36503beffbe
Silva <francisilva@deloitte.pt>
2023-01-26 10:57:02 +0000

    da

--- src/Components/footer/Footer.js
+++ src/Components/footer/Footer.js
@@ -1,22 +1,22 @@
-import { MDBFooter} from 'mdb-react-ui-kit';
+import { MDBFooter } from 'mdb-react-ui-kit';
 
 export const Footer = () => {
-  const Ano = ()=>{
+  const Ano = () => {
     const data = new Date();
     const ano = data.getFullYear();
     return ano;
   }
   const ano = Ano();
-    return(
-        <MDBFooter className='text-center text-white fixed-bottom'  style={{ backgroundColor: '#fff' }}>
+  return (
+    <MDBFooter className='text-center text-white bottom-0 start-0 end-0' style={{ backgroundColor: '#fff' }}>
 
       <div className='text-center p-3' style={{ backgroundColor: 'rgba(0, 0, 0, 0.2)' }}>
         <a className='text-white ' href='/'>
-           SweetDreams
+          SweetDreams
         </a>
         {` - Copyright © ${ano}`}
       </div>
 
     </MDBFooter>
-    )
+  )
 }
